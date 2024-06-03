56235b646379419568d118cc7c878887a4285a4a
Yousef eslam <yousefeslam214@gmail.com>
2024-06-03 21:12:44 +0300

    flask

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,27 +1,31 @@
 #!/usr/bin/python3
-""" Write a script that starts a Flask web application """
+""" 2. Script to start a Flask web application with 3 view functions """
 
 from flask import Flask
+
+
 app = Flask(__name__)
+app.url_map.strict_slashes = False
 
 
-@app.route('/', strict_slashes=False)
+@app.route('/')
 def hello_world():
-    """ return hbnb """
+    """ Returns some text. """
     return 'Hello HBNB!'
 
 
-@app.route('/hbnb', strict_slashes=False)
+@app.route('/hbnb')
 def hello():
-    """ return hbnb """
+    """ Return other text. """
     return 'HBNB'
 
 
-@app.route('/c/<text>', strict_slashes=False)
+@app.route('/c/<text>')
 def c_text(text):
-    """ replace text with variable """
+    """ replace text with variable. """
     text = text.replace('_', ' ')
-    return 'c {}'.format(text)
+    return 'C {}'.format(text)
 
-if __name__ == "__main__":
+
+if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
