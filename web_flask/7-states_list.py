090869d591d4073c5c7c6b5e692bcbabf2ba0cd0
Yousef eslam <yousefeslam214@gmail.com>
2024-06-04 05:51:18 +0300

    goodPhase

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,25 +1,29 @@
 #!/usr/bin/python3
-"""
-start Flask application
-"""
-
-from flask import Flask, render_template
-from models import *
+""" A scrip that starts a flask web app """
 from models import storage
+from models.state import State
+from flask import Flask
+from flask import render_template
 app = Flask(__name__)
+
+
+# Define a function to be called when the application context is torn down
+@app.teardown_appcontext
+def hbnb_db(error):
+    """ remove the current SQLAlchemy Session """
+    storage.close()
 
 
 @app.route('/states_list', strict_slashes=False)
 def states_list():
-    """display a HTML page with the states listed in alphabetical order"""
-    states = sorted(list(storage.all("State").values()), key=lambda x: x.name)
+    # Retrieve all State objects from the storage (presumably a database)
+    states = storage.all(State).values()
+
+    # Sort the list of states based on the 'name'
+    # attribute of each State object
+    states = sorted(states, key=lambda k: k.name)
     return render_template('7-states_list.html', states=states)
 
 
-@app.teardown_appcontext
-def teardown_db(exception):
-    """closes the storage on teardown"""
-    storage.close()
-
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5000')
+    app.run(host='0.0.0.0', port=5000)
